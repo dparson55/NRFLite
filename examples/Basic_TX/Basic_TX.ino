b2d2d04b4c96afcbdd2d32c0a1478d46a117f0d2
dparson55 <dparson55@hotmail.com>
2019-05-23 21:16:33 -0700

    Improved comments, related to #27

--- examples/Basic_TX/Basic_TX.ino
+++ examples/Basic_TX/Basic_TX.ino
@@ -40,10 +40,12 @@ void setup()
     
     // By default, 'init' configures the radio to use a 2MBPS bitrate on channel 100 (channels 0-125 are valid).
     // Both the RX and TX radios must have the same bitrate and channel to communicate with each other.
+    // You can run the 'ChannelScanner' example to help select the best channel for your environment.
     // You can assign a different bitrate and channel as shown below.
-    // _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE250KBPS, 0)
-    // _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE1MBPS, 75)
-    // _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE2MBPS, 100) // THE DEFAULT
+    //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE250KBPS, 0)
+    //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE1MBPS, 75)
+    //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE2MBPS, 100) // THE DEFAULT
+    
     if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN))
     {
         Serial.println("Cannot communicate with radio");
@@ -61,6 +63,14 @@ void loop()
     Serial.print(_radioData.OnTimeMillis);
     Serial.print(" ms");
 
+    // By default, 'send' transmits data and waits for an acknowledgement.  If no acknowledgement is received,
+    // it will try again up to 16 times.  You can also perform a NO_ACK send that does not request an acknowledgement.
+    // The data packet will only be transmitted a single time so there is no guarantee it will be successful.  Any random
+    // electromagnetic interference can sporatically cause packets to be lost, so NO_ACK sends are only suited for certain
+    // types of situations, such as streaming real-time data where performance is more important than reliability.
+    //   _radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData), NRFLite::NO_ACK)
+    //   _radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData), NRFLite::REQUIRE_ACK) // THE DEFAULT
+    
     if (_radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData))) // Note how '&' must be placed in front of the variable name.
     {
         Serial.println("...Success");
@@ -72,11 +82,4 @@ void loop()
     }
 
     delay(1000);
-    
-    /*
-    By default, 'send' transmits data and waits for an acknowledgement.
-    You can also send without requesting an acknowledgement.
-    _radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData), NRFLite::NO_ACK)
-    _radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData), NRFLite::REQUIRE_ACK) // THE DEFAULT
-    */
 }
