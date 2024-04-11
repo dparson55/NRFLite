80b9cad25193ae7b2273f810d6b9cad25ce8bd48
dparson55 <dparson55@hotmail.com>
2024-04-11 13:55:34 -0700

    Update ChannelScanner.ino
    removed some unhelpful comments

--- examples/ChannelScanner/ChannelScanner.ino
+++ examples/ChannelScanner/ChannelScanner.ino
@@ -1,15 +1,8 @@
 /*
 
 Run this to help identify a free channel to use with the radio.  It shows a graph in the serial montior
-which allows you to visualize channels that already have a signal.  You should configure the radio to use
+that allows you to visualize channels that already have a signal.  You should configure the radio to use
 a channel with no existing signals.
-
-By default, 'init' configures the radio to use channel 100 (channels 0-125 are valid).
-Both the RX and TX radios must have the same channel to communicate with each other.
-You can assign a different channel as shown below.
-_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE2MBPS, 0);   // Channel 0
-_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE2MBPS, 75);  // Channel 75
-_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE2MBPS, 100); // Channel 100, THE DEFAULT
 
 Radio    Arduino
 CE    -> 9
