401603fb7a0ae29d2507b43797127190d0073eaa
dparson55 <dparson55@hotmail.com>
2025-02-18 11:29:24 -0800

    Update Sensor_TX_ATtiny85_2Pin.ino

--- examples/Sensor_TX_ATtiny85_2Pin/Sensor_TX_ATtiny85_2Pin.ino
+++ examples/Sensor_TX_ATtiny85_2Pin/Sensor_TX_ATtiny85_2Pin.ino
@@ -1,6 +1,6 @@
 /*
 
-Uses https://github.com/damellis/attiny which requires usage of the Arduino 1.x IDE.
+Uses the board library https://github.com/damellis/attiny which requires usage of the Arduino 1.x IDE.
 
 Demonstrates TX operation with an ATtiny85 using 2 pins for the radio.
 * The ATtiny powers up, takes sensor readings, sends the data, and then powers down.
